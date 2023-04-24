4c840f1099229214136c1e42b52ed54be44da835
saliharya <saliharyazinc@gmail.com>
2023-04-24 14:10:31 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/saliharya/cryptoku/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saliharya.cryptoku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/remote/api_config.dart
@@ -0,0 +1,4 @@
+class ApiConfig {
+  static const apiKey =
+      "849e8775236f9a5552211f58eb1afb83df200151be4c4b57388a38cd5d09557f";
+}
